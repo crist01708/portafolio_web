b4dfe0b1ede4d418229eb2d8a9fea063c3811c3c
Cristóbal Cisterna <cristobal.cisterna1708@gmail.com>
2026-05-21 16:48:25 -0400

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,28 @@
+<footer class="bg-dark text-white text-center py-4">
+
+<div class="container">
+
+<p>
+© 2026 Cristóbal Cisterna -
+Portafolio Profesional
+</p>
+
+<div>
+
+<a href="#" class="text-white me-3">
+GitHub
+</a>
+
+<a href="#" class="text-white me-3">
+LinkedIn
+</a>
+
+<a href="#" class="text-white">
+Correo
+</a>
+
+</div>
+
+</div>
+
+</footer>
---
+++ navbar.php
@@ -0,0 +1,65 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+
+<div class="container">
+
+<a class="navbar-brand" href="#">
+Cristóbal Dev
+</a>
+
+<button class="navbar-toggler"
+type="button"
+data-bs-toggle="collapse"
+data-bs-target="#menu">
+
+<span class="navbar-toggler-icon"></span>
+
+</button>
+
+<div class="collapse navbar-collapse" id="menu">
+
+<ul class="navbar-nav ms-auto">
+
+<li class="nav-item">
+<a class="nav-link" href="#biografia">
+Biografía
+</a>
+</li>
+
+<li class="nav-item">
+<a class="nav-link" href="#habilidades">
+Habilidades
+</a>
+</li>
+
+<li class="nav-item">
+<a class="nav-link" href="#tecnologias">
+Tecnologías
+</a>
+</li>
+
+<li class="nav-item">
+<a class="nav-link" href="#proyectos">
+Proyectos
+</a>
+</li>
+
+<li class="nav-item">
+<a class="nav-link" href="#contacto">
+Contacto
+</a>
+</li>
+
+<li class="nav-item">
+<a class="btn btn-primary ms-2"
+href="login.php">
+Login
+</a>
+</li>
+
+</ul>
+
+</div>
+
+</div>
+
+</nav>
